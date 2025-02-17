b791060c1f39380386ec1f19b9b846a095c2f3f8
supah <andrecostafrancacf@gmail.com>
2025-02-17 18:54:44 -0300

    whatever

--- test1.cpp
+++ test1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
+
 int main(){
-    std::cout << "Hello World!";
-    return 0;
+    int num1, num2;
+    int comp = 20;
+    int sum;
+
+    cout << "Enter the first number" << endl;
+    cin >> num1;
+    cout << "Enter the second number" << endl;
+    cin >> num2;
+
+    sum = num1 + num2;
+    sum >= comp ? cout << "The sum is greater than " << comp << endl
+                : cout << "The sum is less than " << comp << endl;
+    
 }
